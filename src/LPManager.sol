f9f27d934a3a2906843dd0bc214986e52f47d1e0
DrGorilla_md <dr_gorilla@protonmail.com>
2022-06-08 00:04:32 +0200

    still compiles

--- src/LPManager.sol
+++ src/LPManager.sol
@@ -2,38 +2,41 @@
 pragma solidity 0.8.14;
 
 import "@uniswap/v3-periphery/contracts/libraries/LiquidityAmounts.sol";
+import "@uniswap/v3-periphery/contracts/libraries/PoolAddress.sol";
+
 import "@uniswap/v3-core/contracts/libraries/TickMath.sol";
 import "@uniswap/v3-core/contracts/libraries/Position.sol";
 import "@uniswap/v3-core/contracts/interfaces/IUniswapV3Pool.sol";
 
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+interface IWETH9 is IERC20 {
+    /// @notice Deposit ether to get wrapped ether
+    function deposit() external payable;
+
+    /// @notice Withdraw wrapped ether to get ether
+    function withdraw(uint256) external;
+}
+
 contract LPManager {
-    struct Position {
-        int24 tickLower;
-        int24 tickUpper;
-        uint128 amount;
-    }
-
-    // Pool=>Positions[]
-    mapping(IUniswapV3Pool => Position[]) public currentPositions;
-
     address immutable owner;
 
-    constructor() payable {
+    IWETH9 immutable WETH;
+
+    constructor(IWETH9 _WETH) payable {
         owner = msg.sender;
+        WETH = _WETH;
     }
 
     function addLP(
         IUniswapV3Pool _pool,
-        address token0,
-        address token1,
-        uint160 _sqrtPriceX96,
+        uint160 _sqrtPriceX96, // offchain reading to save one call
         uint256 _amount0,
         uint256 _amount1,
         int24 _tickLower,
-        int24 _tickUpper
-    ) external {
+        int24 _tickUpper,
+        bytes calldata _data // abi.encode(address _factory, address _token0, address _token1, uint24 _fee)
+    ) external payable {
         uint128 _liquidity = LiquidityAmounts.getLiquidityForAmounts(
             _sqrtPriceX96,
             TickMath.getSqrtRatioAtTick(_tickLower),
@@ -42,20 +45,50 @@ contract LPManager {
             _amount1
         );
 
-        _pool.mint(
-            address(this),
-            _tickLower,
-            _tickUpper,
-            _liquidity,
-            abi.encode(_pool, token0, token1)
+        _pool.mint(address(this), _tickLower, _tickUpper, _liquidity, _data);
+    }
+
+    function removeLP(
+        int24 tickLower,
+        int24 tickUpper,
+        uint128 amount,
+        IUniswapV3Pool pool,
+        IERC20 token0,
+        IERC20 token1
+    ) external {
+        if (msg.sender != owner) revert();
+
+        pool.collect(
+            owner,
+            tickLower,
+            tickUpper,
+            type(uint128).max,
+            type(uint128).max
         );
 
-        currentPositions[_pool].push(
-            Position({
-                tickLower: _tickLower,
-                tickUpper: _tickUpper,
-                amount: _liquidity
-            })
+        (uint256 amount0, uint256 amount1) = pool.burn(
+            tickLower,
+            tickUpper,
+            amount
+        );
+
+        token0.transfer(owner, amount0);
+        token1.transfer(owner, amount1);
+    }
+
+    function collectFees(
+        int24 tickLower,
+        int24 tickUpper,
+        IUniswapV3Pool pool
+    ) external {
+        if (msg.sender != owner) revert();
+
+        pool.collect(
+            owner,
+            tickLower,
+            tickUpper,
+            type(uint128).max,
+            type(uint128).max
         );
     }
 
@@ -64,17 +97,26 @@ contract LPManager {
         uint256 _amount1Owed,
         bytes calldata _data
     ) external {
-        (address _pool, address _token0, address _token1) = abi.decode(
-            _data,
-            (address, address, address)
+        (address _factory, address _token0, address _token1, uint24 _fee) = abi
+            .decode(_data, (address, address, address, uint24));
+
+        address _pool = PoolAddress.computeAddress(
+            _factory,
+            PoolAddress.getPoolKey(_token0, _token1, _fee)
         );
+
         if (msg.sender != _pool) revert();
 
-        if (_amount0Owed > 0)
-            IERC20(_token0).transferFrom(tx.origin, _pool, _amount0Owed);
+        if (_amount0Owed > 0) {
+            if (_token0 == address(WETH)) {
+                WETH.deposit{value: _amount0Owed}();
+                WETH.transfer(_pool, _amount0Owed);
+            } else IERC20(_token0).transferFrom(tx.origin, _pool, _amount0Owed);
+        }
         if (_amount1Owed > 0)
-            IERC20(_token1).transferFrom(tx.origin, _pool, _amount1Owed);
+            if (_token1 == address(WETH)) {
+                WETH.deposit{value: _amount1Owed}();
+                WETH.transfer(_pool, _amount1Owed);
+            } else IERC20(_token1).transferFrom(tx.origin, _pool, _amount1Owed);
     }
-
-    function collectFees(Position[] calldata _positions) external {}
 }
